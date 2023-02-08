7aca6ba05350da757bf5158e3ded79a76ec1d237
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2023-02-08 09:07:10 -0300

    Configurando a comunicação com o store

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,16 +1,71 @@
 import { ipcMain } from 'electron'
+import { randomUUID } from 'node:crypto'
+
+import {
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  CreateDocumentResponse,
+  FetchAllDocumentsResponse,
+  SaveDocumentRequest,
+  DeleteDocumentRequest,
+  Document,
+} from '@shared/types/ipc'
 import { IPC } from '@shared/constants/ipc'
-import { FetchAllDocumentsResponse } from '@shared/types/ipc'
+
+import { store } from './store'
 
 ipcMain.handle(
   IPC.DOCUMENTS.FETCH_ALL,
   async (): Promise<FetchAllDocumentsResponse> => {
     return {
-      data: [
-        { id: '1', title: 'Document 1', content: 'Content 1' },
-        { id: '2', title: 'Document 2', content: 'Content 2' },
-        { id: '3', title: 'Document 3', content: 'Content 2' },
-      ],
+      data: Object.values(store.get('documents')),
     }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH_ONE,
+  async (_, { id }: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    const document = store.get(`documents.${id}`) as Document
+
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.CREATE,
+  async (): Promise<CreateDocumentResponse> => {
+    const id = randomUUID()
+    const document: Document = {
+      id,
+      title: 'Untitled',
+    }
+
+    store.set(`documents.${id}`, document)
+
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.SAVE,
+  async (_, { id, title, content }: SaveDocumentRequest): Promise<void> => {
+    store.set(`documents.${id}`, {
+      id,
+      title,
+      content,
+    })
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.DELETE,
+  async (_, { id }: DeleteDocumentRequest): Promise<void> => {
+    // @ts-ignore (https://github.com/sindresorhus/electron-store/issues/196)
+    store.delete(`documents.${id}`)
   },
 )
